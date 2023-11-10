9fa2e4a5859c303e7254e209dbeb5897ebb10515
PascalSchwab <blazerclips@gmail.com>
2023-11-10 12:23:16 +0100

    Fetch

---
+++ Aufgabenblatt 3/main.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://online-lectures-cs.thi.de/chat"
+const COLLECTION_ID = "9553730f-9fa6-4e5a-9876-f2d8b5f568f6"
+
+// User Toms
+const USERNAME = "Tom"
+const PASSWORD = "12345678"
+const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5NTM4MjUzfQ.J_A_2fq5URQtKLnC-RNpSNKetU-hBhDBeL39okA6cv0'
+
+// User Jerry
+// const USERNAME = "Jerry"
+// const PASSWORD = "87654321"
+// const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2OTk2MTUzNzB9.xJCPvsF1xD7HKiwLPkmxtys70UWBQFmF3JAbzZstQu0"
