7272eee19d9cec70ce407b6e5f1eb90933adcf55
nik1648 <nik1648@thi.de>
2023-11-13 11:53:28 +0100

    ✨

--- Aufgabenblatt 3/chat.js
+++ Aufgabenblatt 3/chat.js
@@ -23,23 +23,23 @@ window.setInterval(function () {
 
 window.backendUrl = "https://online-lectures-cs.thi.de/chat/7b75c2e2-0dbd-4363-8e97-419ac36ac777";
 window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5NTM4Mzk1fQ.fMSqkUd7KhPW-G01o4f_h3Ai5s8qHsUSK3VkqQ1kCow";
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        let data = JSON.parse(xmlhttp.responseText);
+const xmlhttp1 = new XMLHttpRequest();
+xmlhttp1.onreadystatechange = function () {
+    if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200) {
+        let data = JSON.parse(xmlhttp1.responseText);
         console.log(data);
     }
 };
-xmlhttp.open("GET", backendUrl + "/user", true);
-xmlhttp.setRequestHeader('Authorization', 'Bearer ' + token);
-xmlhttp.send();
+xmlhttp1.open("GET", backendUrl + "/user", true);
+xmlhttp1.setRequestHeader('Authorization', 'Bearer ' + token);
+xmlhttp1.send();
 
-var xmlhttps = new XMLHttpRequest();
-xmlhttps.onreadystatechange = function () {
-    if (xmlhttps.readyState == 4) {
-        if (xmlhttps.status == 204) {
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.onreadystatechange = function () {
+    if (xmlhttp2.readyState == 4) {
+        if (xmlhttp2.status == 204) {
             console.log("Exists");
-        } else if (xmlhttps.status == 404) {
+        } else if (xmlhttp2.status == 404) {
             console.log("Does not exist");
         }
     }
@@ -47,3 +47,31 @@ xmlhttps.onreadystatechange = function () {
 xmlhttps.open("GET", "https://online-lectures-cs.thi.de/chat/7b75c2e2-0dbd-4363-8e97-419ac36ac777/user/Tom", true);
 xmlhttps.send();
 
+var xmlhttp3 = new XMLHttpRequest();
+xmlhttp3.onreadystatechange = function () {
+    if (xmlhttp3.readyState == 4 && xmlhttp3.status == 200) {
+        let data = JSON.parse(xmlhttp3.responseText);
+        console.log(data);
+    }
+};
+xmlhttp3.open("GET", "https://online-lectures-cs.thi.de/chat/ed84cb44-8b4c-4eeb-83d8-7df122c7b164/message/Jerry", true);
+xmlhttp3.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5NjIxNzk0fQ.zfRU6UnRiz853w3CAH4Ke0tADNqk0OxHQgxDFOnrE08');
+xmlhttp3.send();
+
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
+        console.log("done...");
+    }
+};
+xmlhttp.open("POST", "https://online-lectures-cs.thi.de/chat/ed84cb44-8b4c-4eeb-83d8-7df122c7b164/message", true);
+xmlhttp.setRequestHeader('Content-type', 'application/json');
+// Add token, e. g., from Tom
+xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5NjIxNzk0fQ.zfRU6UnRiz853w3CAH4Ke0tADNqk0OxHQgxDFOnrE08');
+// Create request data with message and receiver
+let data = {
+    message: "Hello?!",
+    to: "Jerry"
+};
+let jsonString = JSON.stringify(data); // Serialize as JSON
+xmlhttp.send(jsonString); // Send JSON-data to server
