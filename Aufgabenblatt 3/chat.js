e14f931959c0c013a98c7627cdce63eeb38e74be
lolopion <nicokirn.nk@gmail.com>
2023-11-16 16:06:27 +0100

    ✨

--- Aufgabenblatt 3/chat.js
+++ Aufgabenblatt 3/chat.js
@@ -1,4 +1,6 @@
-let chatData = [];
+let chatData = []; 
+const JERRY_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE3MDAxNDM4NTR9.tOujsKI14Uxl5ZNagmvXALRBJf7WtHuIvXSt2UYDucE";
+
 // function getChatpartner() {
 //     const url = new URL(window.location.href);
 //     const queryParams = url.searchParams;
@@ -13,12 +15,15 @@ let chatData = [];
 //überSchrift.innerText = "Chat with " + friendName;
 
 function loadChat() {
+    let ChatBox = document.getElementById("chatbox");
     getRequest("message/" + USERNAME).then((response)=>{
         if(response.status==200){
             console.log(response.data);
             chatData = response.data;
+            ChatBox = chatData;
         } 
     });
+    
 }
 
 window.setInterval(function () {
@@ -32,57 +37,34 @@ window.setInterval(function () {
 //         console.log(data);
 //     }
 // };
-// xmlhttp1.open("GET", backendUrl + "/user", true);
-// xmlhttp1.setRequestHeader('Authorization', 'Bearer ' + token);
+// xmlhttp1.open("GET", BASE_URL + "/user", true);
+// xmlhttp1.setRequestHeader('Authorization', 'Bearer ' + ACCESS_TOKEN);
 // xmlhttp1.send();
 
-// function UserExists(username) {
-//     var xmlhttp2 = new XMLHttpRequest();
-//     xmlhttp2.onreadystatechange = function () {
-//         if (xmlhttp2.readyState == 4) {
-//             if (xmlhttp2.status == 204) {
-//                 console.log("Exists");
-//             } else if (xmlhttp2.status == 404) {
-//                 console.log("Does not exist");
-//             }
-//         }
-//     };
-//     xmlhttps.open("GET", window.backendUrl + "/user/" + username, true);
-//     xmlhttps.send();
-// }
-
-// var xmlhttp3 = new XMLHttpRequest();
-// xmlhttp3.onreadystatechange = function () {
-//     if (xmlhttp3.readyState == 4 && xmlhttp3.status == 200) {
-//         let data = JSON.parse(xmlhttp3.responseText);
-//         console.log(data);
-//     }
-// };
-// xmlhttp3.open("GET", window.backendUrl + "/message/Jerry", true);
-// xmlhttp3.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5NjIxNzk0fQ.zfRU6UnRiz853w3CAH4Ke0tADNqk0OxHQgxDFOnrE08');
-// xmlhttp3.send();
-
 function sendMessage() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
-            console.log("done...");
+    let newchatData = [];
+    newchatData = document.getElementById("textsubmit");
+    // let xmlhttp = new XMLHttpRequest();
+    // xmlhttp.onreadystatechange = function () {
+    //     if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
+    //         console.log("done...");
+    //     }
+    // };
+    postRequest("message/" + USERNAME, ACCESS_TOKEN).then((response)=>{
+        if(response.status==200){
+            chatData= response.data + newchatData;
         }
-    };
-    postRequest().then((response)=>{
-
     })
     
-    
-    xmlhttp.open("POST", window.backendUrl + "/message", true);
-    xmlhttp.setRequestHeader('Content-type', 'application/json');
-    // Add token, e. g., from Tom
-    xmlhttp.setRequestHeader('Authorization', '');
-    // Create request data with message and receiver
-    let data = {
-        msg: message,
-        to: user
-    };
-    let jsonString = JSON.stringify(data); // Serialize as JSON
-    xmlhttp.send(jsonString); // Send JSON-data to server
+    // xmlhttp.open("POST", window.backendUrl + "/message", true);
+    // xmlhttp.setRequestHeader('Content-type', 'application/json');
+    // // Add token, e. g., from Tom
+    // xmlhttp.setRequestHeader('Authorization', '');
+    // // Create request data with message and receiver
+    // let data = {
+    //     msg: message,
+    //     to: user
+    // };
+    // let jsonString = JSON.stringify(data); // Serialize as JSON
+    // xmlhttp.send(jsonString); // Send JSON-data to server
 }
