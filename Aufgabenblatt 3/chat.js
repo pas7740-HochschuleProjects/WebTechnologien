d8a666cc599ceaca8d694ea132a171a565b9c7e3
nik1648 <nik1648@thi.de>
2023-11-17 15:30:40 +0100

    ✨

--- Aufgabenblatt 3/chat.js
+++ Aufgabenblatt 3/chat.js
@@ -13,13 +13,23 @@ let überSchrift = document.getElementById("heading");
 überSchrift.innerText = "Chat with " + friendName;
 
 function loadChat() {
-    let ChatBox = document.getElementById("chatbox");
-    getRequest("message/Jerry").then((response) => {
+    let chatBox = document.getElementById("chatbox");
+    getRequest("message/" + friendName).then((response) => {
         if (response.status == 200) {
             console.log(response.data);
             chatData = response.data;
+            let arrayLength = response.data.length;
+            for (let j; j < arrayLength; j++) {
+                renderChat(chatBox, chatData[j]);
+            }
         }
     });
+}
+
+function renderChat(chatBox, chatMessage) {
+    document.createElement("li");
+
+    chatBox.appendChild();
 }
 
 window.setInterval(function () {
@@ -29,6 +39,6 @@ window.setInterval(function () {
 function sendMessage() {
     let newchatData = document.getElementById("textsubmit").value;
 
-    postRequest("message", { to: "Jerry", message: newchatData, from: USERNAME });
+    postRequest("message", { to: friendName, message: newchatData, from: USERNAME });
 
 }
