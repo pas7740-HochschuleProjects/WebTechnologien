65cfd11a80bb2e1644b35d8aafe0a297f027c0ce
Marcel <marcel.binder3@gmail.com>
2023-11-21 12:16:00 +0100

    a) fertig e) in bearbeitung i) noch nicht angefangen

--- Aufgabenblatt 4/start.php
+++ Aufgabenblatt 4/start.php
@@ -1,7 +1,13 @@
-// In Progress
-session_start();
-
+<?php
 spl_autoload_register(function($class) {
 include str_replace('\\', '/', $class) . '.php';
 });
 
+session_start();
+
+define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat/');
+define('CHAT_SERVER_ID', 'COLLECTION_ID'); 
+
+// in jede andere php Datei bitte " require("start.php"); " einfügen
+
+?>
