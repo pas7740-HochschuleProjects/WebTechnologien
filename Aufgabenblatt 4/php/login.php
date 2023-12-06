57acada61c0b7171a3c79a3adbce8a5c0923db0f
lolopion <nicokirn.nk@gmail.com>
2023-12-06 15:15:10 +0100

    ✨ login verfeinert

--- Aufgabenblatt 4/php/login.php
+++ Aufgabenblatt 4/php/login.php
@@ -1,16 +1,22 @@
 <?php
 
 require("start.php");
+$username="";
+$password="";
 
 if(isset($_SESSION["user"])){
   header("Location: friends.php");
 }
+if(isset($_POST['username'])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+}
 
-if($service->login("Tom", "12345678")==true){
-  $_SESSION["user"] = new Model\User("Tom");
+if($service->login($username, $password)==true){
+  $_SESSION["user"] = new Model\User($username);
   header("Location: friends.php");
-} else {
-    echo("Login Fehler!");
+} else {  
+  echo("Login Fehler!");
 }
 
 ?>
