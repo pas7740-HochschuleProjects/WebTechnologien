52653af436e0627c8ad3b98ec2ae2bed37d3bf43
PascalSchwab <blazer0HD@gmail.com>
2023-12-06 16:42:58 +0100

    User Exists js

--- Aufgabenblatt 4/js/register.js
+++ Aufgabenblatt 4/js/register.js
@@ -28,7 +28,7 @@ form.addEventListener('input', (e) => {
     }
 
     // Check if Username is already used
-    sendRequest(REQUEST_TYPE.GET, "user/" + username.value, undefined, false).then((response)=>{
+    phpRequest(REQUEST_TYPE.GET, "http://localhost/php/ajax_check_user", undefined, false).then((response)=>{
         if (response.status == 204) {
             UserMessages.push('Username is already used');
             document.getElementById("username").style.border = "2px solid red";
