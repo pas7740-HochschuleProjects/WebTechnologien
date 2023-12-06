71b1f4adcb67a02c185cccc2af52b7cfc1ad8fc2
PascalSchwab <blazer0HD@gmail.com>
2023-12-06 16:31:03 +0100

    🚧 Register

--- Aufgabenblatt 4/js/request.js
+++ Aufgabenblatt 4/js/request.js
@@ -13,6 +13,38 @@ async function getRequest(endpoint){
     return await sendRequest(REQUEST_TYPE.GET, endpoint, undefined, true);
 }
 
+async function phpRequest(requestType, url, body, auth){
+    let requestBody = (typeof body !== "undefined") ? JSON.stringify(body) : undefined;
+    let tokenHeader =
+    {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN
+        },
+        method: requestType,
+        body: requestBody
+    };
+    let basicHeader =
+    {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: requestType,
+        body: requestBody
+    };
+    return await fetch(url, (auth) ? tokenHeader : basicHeader)
+        .then(async (response) => {
+            let data = await response.json().catch((error)=>{});
+            return {
+                data: data,
+                status: response.status
+            };
+        })
+        .catch((error) => {
+            console.error("Fetch error: " + error);
+        });
+}
+
 async function sendRequest(requestType, endpoint, body, auth) {
     let url = BASE_URL + "/" + COLLECTION_ID + "/" + endpoint;
     let requestBody = (typeof body !== "undefined") ? JSON.stringify(body) : undefined;
--- Aufgabenblatt 4/php/register.php
+++ Aufgabenblatt 4/php/register.php
@@ -2,4 +2,65 @@
 
 require("start.php");
 
+// check if empty
+if(empty($_POST["username"])){
+   header("Location: /register.html");
+}
+if(empty($_POST["password"])){
+   header("Location: /register.html");
+}
+if(empty($_POST["confirm"])){
+   header("Location: /register.html");
+}
+
+
+$username= $_POST['username'];
+$password= $_POST['password'];
+$confirmpassword= $_POST['confirm'];
+
+$usernamelength= strlen($username);
+$passwordlength= strlen($password);
+
+$a;
+$b;
+
+// Username Validation
+
+if($usernamelength < 3) {
+    echo("Username requires at least 3 characters");
+    header("Location: /register.html");
+}
+else {
+    $a = true;
+}
+
+// PW Validation
+if($passwordlength < 8) {
+    echo("Password requires at least 8 characters");
+    header("Location: /register.html");
+}
+else {
+    if($password != $confirmpassword){
+        echo("Password requires at least 8 characters");
+        header("Location: /register.html");
+    }
+    else {
+        $b = true;
+    }
+}
+
+
+
+if ($a && $b) {
+    if ($service->register($username, $password) == true) {
+        $_SESSION["user"] = new Model\User($username);
+        header("Location: friends.php");
+    } else {
+        echo ("Register Error!");
+    }
+}
+else {
+    echo ("Validation failed!");
+}
+
 ?>
