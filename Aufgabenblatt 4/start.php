727dd8f3064a36db6f2e49785ccb1166a6eb22cb
PascalSchwab <blazer0HD@gmail.com>
2023-12-11 01:33:33 +0100

    New collection id

--- Aufgabenblatt 4/start.php
+++ Aufgabenblatt 4/start.php
@@ -8,7 +8,7 @@ spl_autoload_register(function($class) {
 session_start();
 
 define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat/');
-define('CHAT_SERVER_ID', '583eac22-e656-4de7-8cde-ff360b3516a3');
+define('CHAT_SERVER_ID', '44546a7d-74bf-4a1c-8458-a4c71d553fad');
 
 $service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
 
