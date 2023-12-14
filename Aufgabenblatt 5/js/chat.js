75c64af5ee3debdf701d5abb617eda5a94b12a9f
nik1648 <nik1648@thi.de>
2023-12-14 10:32:46 +0100

    🚧

--- Aufgabenblatt 5/js/chat.js
+++ Aufgabenblatt 5/js/chat.js
@@ -61,7 +61,6 @@ function renderChat(chatBox, chatMessage, id) {
 
     let time = new Date(chatMessage.time).toLocaleTimeString();
     liElement.children[1].innerText = time;
-    liElement.children[1].getElementById(id).classList.add("position-absolute top-100 start-100 translate-middle");
 
     chatBox.appendChild(liElement);
 }
