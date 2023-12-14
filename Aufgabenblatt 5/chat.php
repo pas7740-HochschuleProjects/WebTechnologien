5d86dd874427d8923fe06a4f0bcfd68e73aee73a
nik1648 <nik1648@thi.de>
2023-12-14 08:39:29 +0100

    danger???

--- Aufgabenblatt 5/chat.php
+++ Aufgabenblatt 5/chat.php
@@ -36,7 +36,7 @@ if(empty($_SESSION["user"])){
      <a class="btn btn-secondary" href="profile.php?friend=<?php echo $_GET['friend']; ?>">Profile</a> 
             <form method="post" action="friends.php" id="remove-friend-form">
                 <input type="hidden" value="<?php echo $_GET['friend']; ?>" name="friendname" />
-                <button type="submit" name="action" class="btn btn.bg-danger" value="delete-friend" id="remove-friend-button">
+                <button type="submit" name="action" class="btn btn-primary .bg-danger" value="delete-friend" id="remove-friend-button">
                     <a class="red-link">
                         Remove Friend
                     </a>
